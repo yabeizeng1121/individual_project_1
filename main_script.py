a67c7e9c65f0ec8eff54ed8b5f877e440998c079
yabeizeng1121 <143656459+yabeizeng1121@users.noreply.github.com>
2023-09-19 17:16:22 -0400

    Create main_script.py

---
+++ main_script.py
@@ -0,0 +1,20 @@
+import polars as pl  
+from lib import *  
+
+def main():
+
+    data_path = "cars.csv"
+    df = pl.read_csv(data_path)  
+
+    mean_values = compute_mean(df)
+    median_values = compute_median(df)
+    mode_values = compute_mode(df)
+    std_dev_values = compute_std_dev(df)
+
+    print("Mean Values:", mean_values)
+    print("Median Values:", median_values)
+    print("Mode Values:", mode_values)
+    print("Standard Deviation Values:", std_dev_values)
+
+if __name__ == "__main__":
+    main()
