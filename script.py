01b36fd5ee5ca6cd60979d6cadf0d18a8a636bd0
yabeizeng1121 <143656459+yabeizeng1121@users.noreply.github.com>
2023-09-19 21:07:42 -0400

    Update script.py

--- script.py
+++ script.py
@@ -1,20 +1,14 @@
 import pandas as pd
-from lib 
+from lib import data_load, data_summary, data_visual
 
 def main():
 
     data_path = "cars.csv"
     df = pd.read_csv(data_path, sep=';')
+    sum_data = data_summary(df)
 
-    mean_values = compute_mean(df)
-    median_values = compute_median(df)
-    mode_values = compute_mode(df)
-    std_dev_values = compute_std_dev(df)
+    print(sum_data)
 
-    print("Mean Values:", mean_values)
-    print("Median Values:", median_values)
-    print("Mode Values:", mode_values)
-    print("Standard Deviation Values:", std_dev_values)
 
 if __name__ == "__main__":
     main()
