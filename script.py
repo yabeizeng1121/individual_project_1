a7dd1276fc32e238a224764769240f84c9de8175
yabeizeng1121 <143656459+yabeizeng1121@users.noreply.github.com>
2023-09-19 21:27:15 -0400

    Update script.py

--- script.py
+++ script.py
@@ -1,4 +1,4 @@
-import pandas as pd
+# import pandas as pd
 from lib import load_data, data_summary, data_visual
 
 def main():
@@ -8,6 +8,7 @@ def main():
     sum_data = data_summary(df)
 
     print(sum_data)
+    data_visual(df)
 
 
 if __name__ == "__main__":
